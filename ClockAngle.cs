41112e1bf1ae49478cba95661a6bc98861549201
NourhanAhmed24 <83145003+NourhanAhmed24@users.noreply.github.com>
2023-07-01 00:06:36 +0300

    Add files via upload
    Solution of first task "angle between hours arrow and minutes arrow" : ClockAngle.cs
    Solution of second task "Tree depth" : Depth.cs

---
+++ ClockAngle.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        public static int calculateAngle(int hours, int minutes)
+        {
+            if(hours > 12 || minutes>60 || minutes<0 || hours<0)
+            {
+                Console.Write("wrong inputs, please try again.");
+            }
+
+            if(minutes==60)
+            {
+                minutes = 0;
+                hours = hours + 1;
+
+                if(hours>12)
+                {
+                    hours = hours - 12;
+                }
+
+            }
+
+            if(hours==12)
+            {
+                hours = 0;
+            }
+
+
+
+            //Get hours angle
+            float hDegrees = 360.0f / (12.0f * 60.0f);
+            int hAngle =(int) (((hours * 60) +minutes) * hDegrees) ;
+
+            //Get minutes angle
+            double mDegrees = (double)(360.0f / 60.0f);
+            int mAngle = (int)(minutes * mDegrees);
+
+            //Get difference between 2 angles
+            int diff = Math.Abs(hAngle - mAngle);
+            diff = Math.Min(360 - diff, diff);
+       
+            return diff;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(calculateAngle(12, 30));
+            Console.Write(calculateAngle(3, 30));
+
+        }
+
+
+      
+    }
+}
+
---
+++ Depth.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Branch
+{
+    private List<Branch> branches;
+
+    public List<Branch> GetBranches()
+    {
+        return branches;
+    }
+
+    public Branch()
+    {
+        this.branches = new List<Branch>();
+    }
+
+    public void AddBranch(Branch branch)
+    {
+        branches.Add(branch);
+    }
+}
+
+public class Class2
+{
+    public static int Depth(Branch branch)
+    {
+        int depth = 0;
+        if (branch.GetBranches().Count == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            List<Branch> childBranches = branch.GetBranches();
+            for (int i = 0; i < childBranches.Count; i++)
+            {
+                int childDepth = Depth(childBranches[i]);
+                if (childDepth > depth)
+                {
+                    depth = childDepth;
+                }
+            }
+        }
+
+        //add 1 to add the root node
+        return depth + 1;
+    }
+
+    public static void Main(string[] args)
+    {
+        // Creating the structure
+        Branch root = new Branch();
+
+        Branch branch1 = new Branch();
+        Branch branch2 = new Branch();
+        Branch branch3 = new Branch();
+        Branch branch4 = new Branch();
+        Branch branch5 = new Branch();
+        Branch branch6 = new Branch();
+        Branch branch7 = new Branch();
+        Branch branch8 = new Branch();
+
+        root.AddBranch(branch1);
+        root.AddBranch(branch2);
+
+        branch1.AddBranch(branch3);
+        branch1.AddBranch(branch4);
+        branch2.AddBranch(branch5);
+
+        branch3.AddBranch(branch6);
+        branch5.AddBranch(branch7);
+        branch7.AddBranch(branch8);
+
+        // Calculating the depth
+        int depth = Depth(root);
+
+        Console.Write("Depth: " + depth); // Output: Depth: 5
+    }
+}
